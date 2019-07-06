0796f177126853d019636c47e4b000b487e3551b
Conrad Bituin <inbetweenwebdesigns@gmail.com>
2019-07-06 13:41:22 +0000

    Automatically backed up by Learn

--- db/migrate/20180314210852_create_users.rb
+++ db/migrate/20180314210852_create_users.rb
@@ -3,8 +3,9 @@ class CreateUsers < ActiveRecord::Migration[5.1]
     create_table :users do |t|
       t.string :username
       t.string :password_digest
+    end
   end
-  
+
   def down
   end
 end
